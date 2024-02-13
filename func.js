38385c116fc15f434f3ba556c4785673f7efb2dd
Kataev <Kataev.hamzat@mail.ru>
2024-02-14 02:09:49 +0300

    a

--- func.js
+++ func.js
@@ -35,3 +35,58 @@ function generateSongText(bottles) {
   }
   
 
+
+
+
+  function generateSongText(numBottles) {
+    let songText = "";
+    
+    for (let i = numBottles; i > 0; i--) {
+      let bottlesWord = i === 1 ? "бутылка" : i === 2 ? "бутылки" : "бутылок";
+      let beerWord = i === 1 ? "пиво" : "пива";
+      
+      songText += `${i} ${bottlesWord} ${beerWord} на стене, ${i} ${bottlesWord} ${beerWord}!\n`;
+      songText += `Возьми одну, пусти по кругу, ${i - 1} ${i - 1 === 1 ? "бутылка" : "бутылки"} ${beerWord} на стене!\n\n`;
+    }
+    
+    return songText;
+  }
+  
+  
+
+  
+  
+  function generateSongText(number) {
+    const verse = (num) => {
+      if (num === 0) {
+        return "Нет бутылок пива на стене!";
+      } else if (num === 1) {
+        return "1 бутылка пива на стене!";
+      } else if (num % 10 === 1 && num !== 11) {
+        return `${num} бутылка пива на стене!`;
+      } else if (num % 10 >= 2 && num % 10 <= 4 && (num < 10 || num > 20)) {
+        return `${num} бутылки пива на стене!`;
+      } else {
+        return `${num} бутылок пива на стене!`;
+      }
+    };
+  
+    let songText = "";
+    for (let i = number; i >= 0; i--) {
+      const currentVerse = verse(i);
+      songText += currentVerse + "\n";
+      if (i !== 0) {
+        songText += "Возьми одну, пусти по кругу, ";
+        songText += verse(i - 1) + "\n\n";
+      }
+    }
+  
+    return {
+      text: songText,
+      verses: verse,
+    };
+  }
+  
+  const song = generateSongText(99);
+  console.log(song.text);
+  console.log(song.verses(5));
